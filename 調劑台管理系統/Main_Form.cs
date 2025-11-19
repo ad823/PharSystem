581d894d42562a89d98042426654482cd167f0fb
部立澎湖 <HS@WIN-R5GFI1D9AR0>
2025-11-20 00:24:44 +0800

    20251120

--- 調劑台管理系統/Main_Form.cs
+++ 調劑台管理系統/Main_Form.cs
@@ -21,8 +21,8 @@ using MyPrinterlib;
 using MyOffice;
 using HIS_DB_Lib;
 using H_Pannel_lib;
-[assembly: AssemblyVersion("1.0.25.11171")]
-[assembly: AssemblyFileVersion("1.0.25.11171")]
+[assembly: AssemblyVersion("1.0.25.11192")]
+[assembly: AssemblyFileVersion("1.0.25.11192")]
 namespace 調劑台管理系統
 {
 
