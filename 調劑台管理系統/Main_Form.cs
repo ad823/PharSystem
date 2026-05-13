fb28628047216c55799d8bae3492ec402197e0c0
ad823 <Hson_Evan@outlook.com>
2026-05-13 13:29:24 +0800

    20250513

--- 調劑台管理系統/Main_Form.cs
+++ 調劑台管理系統/Main_Form.cs
@@ -21,8 +21,8 @@ using MyPrinterlib;
 using MyOffice;
 using HIS_DB_Lib;
 using H_Pannel_lib;
-[assembly: AssemblyVersion("1.0.26.05121")]
-[assembly: AssemblyFileVersion("1.0.26.05121")]
+[assembly: AssemblyVersion("1.0.26.05131")]
+[assembly: AssemblyFileVersion("1.0.26.05131")]
 namespace 調劑台管理系統
 {
 
